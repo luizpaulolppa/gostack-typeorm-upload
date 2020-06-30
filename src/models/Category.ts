82927822f86358b48053450cb1d19f3199cf0454
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-06-29 23:28:03 -0300

    create new association

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -3,8 +3,10 @@ import {
   Column,
   PrimaryGeneratedColumn,
   CreateDateColumn,
-  UpdateDateColumn
+  UpdateDateColumn,
+  OneToMany
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -15,12 +17,15 @@ class Category {
   @Column()
   title: string;
 
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
+
   @CreateDateColumn()
   created_at: Date;
 
   @UpdateDateColumn()
   updated_at: Date;
-  
+
 }
 
 export default Category;
